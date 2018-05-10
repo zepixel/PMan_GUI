77d539f217c19fa0f8c80d9c5dd90773f687f8be
zepixel <pixel@htkc.org>
2018-05-11 01:48:56 +0200

    init

---
+++ __main__.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+
+class session_manager
+
+
+
+class PmanApp(App):
+    def build(self):
+        return GUI
+
+
+
