51a5ee9fb7dcd3d9a1f63f6ad622fe42d9b1b439
zepixel <pixel@htkc.org>
2018-05-17 02:54:05 +0200

    Add Basic Widget structure

--- __main__.py
+++ __main__.py
@@ -1,12 +1,13 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
-from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import *
 from kivy.core.window import Window
+from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 
 class Main_body(BoxLayout):
     # Corps principal global de l'appli.
@@ -17,13 +18,29 @@ class Main_header(BoxLayout):
     # Barre de header: onglets, info etc.
     pass
 
-class Project_Manager(BoxLayout):
-    # Barre laterale de gestion et recherche des projets.
+
+class Main_ScreenManager(ScreenManager):
+    # ScreenManager pour switcher de eval a render.
     pass
+
+class Project_Manager(BoxLayout):
+    # Barre laterale de gestion et recherche des projets. Contient Project_Search. et Project_Display.
+    pass
+
+class Project_Search(Widget):
+    # Zone de recherche des projets.
+    pass
+
+class Project_Display(BoxLayout):
+    # Lieu d'affichage des objets projets
+    pass
+
 
 class Eval_Screen(Screen):
     # Ecran d'évaluation des projets.
     pass
+
+
 
 class Main_Footer(BoxLayout):
     # Pied de page.
