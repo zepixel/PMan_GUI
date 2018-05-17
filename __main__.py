8e297b91d578d49ddb89fe77e22d0d6077ce529c
zepixel <pixel@htkc.org>
2018-05-17 02:05:45 +0200

    UP

--- __main__.py
+++ __main__.py
@@ -5,15 +5,34 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import *
-
-class Main_header(BoxLayout):
-    pass
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 class Main_body(BoxLayout):
+    # Corps principal global de l'appli.
     pass
 
-class PmanApp(App):
 
+class Main_header(BoxLayout):
+    # Barre de header: onglets, info etc.
+    pass
+
+class Project_Manager(BoxLayout):
+    # Barre laterale de gestion et recherche des projets.
+    pass
+
+class Eval_Screen(Screen):
+    # Ecran d'évaluation des projets.
+    pass
+
+class Main_Footer(BoxLayout):
+    # Pied de page.
+    pass
+
+
+class PmanApp(App):
+    # main app.
+    Window.borderless = True
     def build(self):
         body = Main_body()
         return body
